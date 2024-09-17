af62ffe3233aa32b5be0d6f4cd12aa83a320647c
Bibhab Basnet <bbobbasnet@gmail.com>
2024-09-18 01:01:20 +0545

    aba xai ksm vayo

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch(`https://ezinventory.pythonanywhere.com/login/`, {
+      const response = await fetch(`https://ezinventory.pythonanywhere.com/userauth/login/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
