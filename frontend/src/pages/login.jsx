e1ad7c084cbd50de2bc6c0cbcff2c007d2c5b0c7
Bibhab Basnet <bbobbasnet@gmail.com>
2024-09-18 00:53:18 +0545

    url update final

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -24,7 +24,7 @@ const Login = () => {
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch(`https://ezinventory.pythonanywhere.com/`, {
+      const response = await fetch(`https://ezinventory.pythonanywhere.com/login/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
