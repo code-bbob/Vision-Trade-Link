917302affd3955ee3139767d2c7f91f124a922bc
Bibhab Basnet <bbobbasnet@gmail.com>
2025-07-05 16:39:25 +0545

    bug fix

--- frontend/src/pages/login.jsx
+++ frontend/src/pages/login.jsx
@@ -14,14 +14,14 @@ const Login = () => {
   const [isLoading,setIsLoading] = useState(false)
   const navigate = useNavigate('');
   const dispatch = useDispatch();
-  const url = `http://127.0.0.1:8000/userauth/login/`;
+  const url = `https://ezinventory.pythonanywhere.com/userauth/login/`;
 
   const handleSubmit = async (e) => {
     
     e.preventDefault();
     setIsLoading(true);
     try {
-      const response = await fetch(`http://127.0.0.1:8000/userauth/login/`, {
+      const response = await fetch(`https://ezinventory.pythonanywhere.com/userauth/login/`, {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
