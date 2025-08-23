668d985117ea4375ae21bfa30fa61ea4e019a55f
Bibhab Basnet <bbobbasnet@gmail.com>
2025-08-23 09:53:01 +0545

    bug fix

--- backend/alltransactions/models.py
+++ backend/alltransactions/models.py
@@ -172,7 +172,7 @@ class VendorTransactions(models.Model):
     cheque_number = models.CharField(max_length=255,null=True,blank=True)
     cashout_date = models.DateField(null=True)
     enterprise = models.ForeignKey('enterprise.Enterprise', on_delete=models.CASCADE,related_name='all_vendor_transactions')
-    desc = models.TextField()
+    desc = models.TextField(null=True, blank=True)
     branch = models.ForeignKey('enterprise.Branch', on_delete=models.CASCADE, null=True, blank=True)
     purchase_transaction = models.ForeignKey(PurchaseTransaction, on_delete=models.CASCADE,related_name="vendor_transaction",null=True,blank=True)
     base = models.BooleanField(default=False)
