72b4963c087e4e0b7342f492254221509fc9b2c3
code-bbob <officialbibhab@gmail.com>
2025-08-25 14:08:00 +0545

    Remove phone number filtering from SalesTransactionView search functionality

--- backend/alltransactions/views.py
+++ backend/alltransactions/views.py
@@ -173,7 +173,6 @@ class SalesTransactionView(APIView):
         if search:
             product_transactions = transactions.filter(sales__product__name__startswith = search)
             customer_transactions = transactions.filter(debtor__name__icontains = search)
-            phone_transactions = transactions.filter(phone_number__icontains = search)
             bill_number_transactions = transactions.filter(bill_number__iexact = search)
             transactions = product_transactions.union(customer_transactions,phone_transactions,bill_number_transactions)
 
